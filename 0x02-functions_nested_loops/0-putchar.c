ff0cf36c55fbc8c332d6559518ca8358eacab065
bigSamBoye0768 <samueldoku0768@gmail.com>
2022-03-16 20:43:05 +0000

    Add file of task 0 and README.md file to project

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"                                                                                                               
+                                                                                                                                
+/**                                                                                                                             
+ * main - prints _putchar, followed by a new line.                                                                              
+ * Return: Always (0);                                                                                                          
+ */                                                                                                                             
+int main(void)                                                                                                                  
+{                                                                                                                               
+        char ch[] = "_putchar";                                                                                                 
+                                                                                                                                
+        int i = 0;                                                                                                              
+                                                                                                                                
+        for (; i < 8; i++)                                                                                                      
+                _putchar(ch[i]);                                                                                                
+        _putchar(10);                                                                                                           
+        return (0);                                                                                                             
+}                     
