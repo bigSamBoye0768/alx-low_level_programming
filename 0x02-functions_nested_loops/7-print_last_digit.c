5f01f11ea0c84a2497378066372ffad0a8d93760
bigSamBoye0768 <samueldoku0768@gmail.com>
2022-03-17 14:36:20 +0000

    Add task 7 file to project

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,16 +3,14 @@
 /**
  * print_last_digit - prints the last digit of a number
  * @n: takes in an interger
- * Return: Always 0 (sucess)
+ * Return: Always output (sucess)
 */
 int print_last_digit(int n)
 {
 	int ln = n % 10;
 
 	if (n < 0)
-	{
 		ln = ln * -1;
-	}
-	_putchar('0' + ln);
+	_putchar(ln + '0');
 	return (ln);
 }
