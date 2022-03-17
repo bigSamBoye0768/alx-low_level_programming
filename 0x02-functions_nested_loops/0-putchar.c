bee8e76d18ea58631f93b092b754455718972366
bigSamBoye0768 <samueldoku0768@gmail.com>
2022-03-17 02:39:04 +0000

    Add style to task 0 file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,16 @@
-#include "main.h"                                                                                                               
-                                                                                                                                
-/**                                                                                                                             
- * main - prints _putchar, followed by a new line.                                                                              
- * Return: Always (0);                                                                                                          
- */                                                                                                                             
-int main(void)                                                                                                                  
-{                                                                                                                               
-        char ch[] = "_putchar";                                                                                                 
-                                                                                                                                
-        int i = 0;                                                                                                              
-                                                                                                                                
-        for (; i < 8; i++)                                                                                                      
-                _putchar(ch[i]);                                                                                                
-        _putchar(10);                                                                                                           
-        return (0);                                                                                                             
-}                     
+#include "main.h"
+
+/**
+ * main - prints _putchar, followed by a new line.
+ * Return: Always (0);
+ */
+int main(void)
+{
+	char ch[] = "_putchar";
+	int i = 0;
+
+	for (; i < 8; i++)
+		_putchar(ch[i]);
+	_putchar(10);
+	return (0);
+}
